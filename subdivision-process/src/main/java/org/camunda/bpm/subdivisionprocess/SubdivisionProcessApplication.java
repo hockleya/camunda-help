14414aaee062ada113f60c45dee02db51ca58892
Hockleya <alex.hockley@qv.co.nz>
2017-12-21 11:34:07 +1300

    Attempting project setup

--- subdivision-process/src/main/java/org/camunda/bpm/subdivisionprocess/SubdivisionProcessApplication.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.bpm.subdivisionprocess;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Subdivision Process App")
-public class SubdivisionProcessApplication extends ServletProcessApplication {
-  // empty implementation
-}
